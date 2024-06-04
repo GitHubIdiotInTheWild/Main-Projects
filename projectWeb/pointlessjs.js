5b7f0939acd848a009b577ba33e57311b272e4d9
Terracotta <muradmannaa@gmail.com>
2024-06-04 08:50:46 +0300

    Create pointlessjs.js

---
+++ projectWeb/pointlessjs.js
@@ -0,0 +1,27 @@
+
+const Variable = 'variable';
+
+
+function Function() {
+
+}
+
+
+Function();
+
+
+console.log('message');
+
+
+const Object = {
+    prop1: 'prop1',
+    prop2: 'prop2'
+};
+
+
+for (let i = 0; i < 3; i++) {
+    
+}
+
+
+export default Variable;
